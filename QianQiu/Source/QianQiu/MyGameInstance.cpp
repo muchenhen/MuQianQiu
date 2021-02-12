385275a17d1e93e2f835c2f7970cf8a36d52e822
muchenhen <muchenhen@gmail.com>
2021-02-13 01:26:01 +0800

    0.03
    gameinstance

---
+++ QianQiu/Source/QianQiu/MyGameInstance.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGameInstance.h"
+#include "Misc/Paths.h"
+#include "HAL/PlatformFileManager.h"
+#include "GenericPlatform/GenericPlatformFile.h"
+#include "HAL/FileManager.h"
+
+
+
+// read file content
+static uint8* ReadFile(IPlatformFile& PlatformFile, FString path, uint32& len) {
+	IFileHandle* FileHandle = PlatformFile.OpenRead(*path);
+	if (FileHandle) {
+		len = (uint32)FileHandle->Size();
+		uint8* buf = new uint8[len];
+
+		FileHandle->Read(buf, len);
+
+		// Close the file again
+		delete FileHandle;
+
+		return buf;
+	}
+
+	return nullptr;
+}
+
+UMyGameInstance::UMyGameInstance() :state("main", this) {
+
+}
+
+void UMyGameInstance::Init()
+{
+	state.onInitEvent.AddUObject(this, &UMyGameInstance::LuaStateInitCallback);
+	state.init();
+
+	state.setLoadFileDelegate([](const char* fn, FString& filepath)->TArray<uint8> {
+
+		IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+		FString path = FPaths::ProjectContentDir();
+		FString filename = UTF8_TO_TCHAR(fn);
+		path /= "Lua";
+		path /= filename.Replace(TEXT("."), TEXT("/"));
+
+		TArray<uint8> Content;
+		TArray<FString> luaExts = { UTF8_TO_TCHAR(".lua"), UTF8_TO_TCHAR(".luac") };
+		for (auto& it : luaExts) {
+			auto fullPath = path + *it;
+
+			FFileHelper::LoadFileToArray(Content, *fullPath);
+			if (Content.Num() > 0) {
+				filepath = fullPath;
+				return MoveTemp(Content);
+			}
+		}
+
+		return MoveTemp(Content);
+		});
+}
+
+void UMyGameInstance::Shutdown()
+{
+	state.close();
+}
+
+static int32 PrintLog(NS_SLUA::lua_State* L)
+{
+	FString str;
+	size_t len;
+	const char* s = luaL_tolstring(L, 1, &len);
+	if (s) str += UTF8_TO_TCHAR(s);
+	NS_SLUA::Log::Log("PrintLog %s", TCHAR_TO_UTF8(*str));
+	return 0;
+}
+
+void UMyGameInstance::LuaStateInitCallback()
+{
+	NS_SLUA::lua_State* L = state.getLuaState();
+	lua_pushcfunction(L, PrintLog);
+	lua_setglobal(L, "PrintLog");
+}
---
+++ QianQiu/Source/QianQiu/testGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "testGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QIANQIU_API UtestGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
