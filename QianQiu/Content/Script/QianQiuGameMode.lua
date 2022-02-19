02f1030e5813ac6551b86c2094877658943e04af
muchenhen <muchenhen@gmail.com>
2022-02-19 16:20:58 +0800

    edit

---
+++ QianQiu/Content/Script/QianQiuGameMode.lua
@@ -0,0 +1,38 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+require "UnLua"
+
+local QianQiuGameMode = Class()
+
+function QianQiuGameMode:Initialize(Initializer)
+    print("QianQiuGameMode Initializer")
+end
+
+--function QianQiuGameMode:UserConstructionScript()
+--end
+
+--function QianQiuGameMode:ReceiveBeginPlay()
+--end
+
+--function QianQiuGameMode:ReceiveEndPlay()
+--end
+
+-- function QianQiuGameMode:ReceiveTick(DeltaSeconds)
+-- end
+
+--function QianQiuGameMode:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+--end
+
+--function QianQiuGameMode:ReceiveActorBeginOverlap(OtherActor)
+--end
+
+--function QianQiuGameMode:ReceiveActorEndOverlap(OtherActor)
+--end
+
+return QianQiuGameMode
---
+++ QianQiu/Source/QianQiu/Public/CardManager.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QIANQIU_API CardManager : UObject
+{
+
+	GENERATED_BODY()
+
+public:
+	CardManager();
+	~CardManager();
+};
