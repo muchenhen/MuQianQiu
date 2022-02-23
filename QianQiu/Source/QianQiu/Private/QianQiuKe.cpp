0016398889a6eabc22fc0b95f5bac0e6b56d4ce8
吕柠 <muchenhen@gmail.com>
2022-02-23 14:00:27 +0800

    #4 创建类

---
+++ QianQiu/Source/QianQiu/Private/QianQiuKe.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QianQiuKe.h"
+
+// Sets default values
+AQianQiuKe::AQianQiuKe()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AQianQiuKe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AQianQiuKe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AQianQiuKe::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
