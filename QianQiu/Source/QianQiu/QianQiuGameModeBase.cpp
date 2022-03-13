35f2c5e2458d151d09f189b7b0aca6f054f53d32
muchenhen <muchenhen@gmail.com>
2022-03-13 12:53:28 +0800

    edit

--- QianQiu/Source/QianQiu/QianQiuGameModeBase.cpp
+++ QianQiu/Source/QianQiu/QianQiuGameModeBase.cpp
@@ -6,16 +6,16 @@
 
 void AQianQiuGameModeBase::StartPlay() {
     Super::StartPlay();
-    UWorld* World = GetWorld();
-    FVector Pos(0,0,0);
-    FRotator Rot(0,0,0);
-    if(World)
-    {
-        // FSoftObjectPath BP_Card_Class(FString("/Game/Model/BP_Card"));
-        UClass* BP_Card_Class = StaticLoadClass(AActor::StaticClass(),nullptr,TEXT("Blueprint'/Game/Model/BP_Card.BP_Card_C'"));
-        // auto obj = BP_Card_Class.ResolveObject();
-        
-        World->SpawnActor<AActor>(BP_Card_Class, Pos, Rot);
-
-    }
+    // UWorld* World = GetWorld();
+    // FVector Pos(0,0,0);
+    // FRotator Rot(0,0,0);
+    // if(World)
+    // {
+    //     // FSoftObjectPath BP_Card_Class(FString("/Game/Model/BP_Card"));
+    //     UClass* BP_Card_Class = StaticLoadClass(AActor::StaticClass(),nullptr,TEXT("Blueprint'/Game/Model/BP_Card.BP_Card_C'"));
+    //     // auto obj = BP_Card_Class.ResolveObject();
+    //     
+    //     World->SpawnActor<AActor>(BP_Card_Class, Pos, Rot);
+    //
+    // }
 }
