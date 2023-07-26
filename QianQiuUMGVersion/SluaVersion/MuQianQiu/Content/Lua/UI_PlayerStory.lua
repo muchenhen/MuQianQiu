fd738cbe439a4b5f006683e449b7a242ebb2da4f
木尘痕 <44308876+muchenhen@users.noreply.github.com>
2023-07-26 22:07:31 +0800

    更新
    对已完成和未完成的组合进行了去重
    更新卡面状态，将未获得的卡置灰

--- QianQiuUMGVersion/SluaVersion/MuQianQiu/Content/Lua/UI_PlayerStory.lua
+++ QianQiuUMGVersion/SluaVersion/MuQianQiu/Content/Lua/UI_PlayerStory.lua
@@ -17,6 +17,28 @@ function UI_PlayerStory:InitCards(Story)
     end
 end
 
+function UI_PlayerStory:UpdateCardHaveState(Player)
+    local Cards = {}
+    if Player == EPlayer.PlayerA then
+        Cards = GameManager:GetPlayerADealCards()
+    elseif Player == EPlayer.PlayerB then
+        Cards = GameManager:GetPlayerBDealCards()
+    end
+    local WidgetCards = self.HorizontalBox_Cards:GetAllChildren()
+    for i=0, WidgetCards:Num()-1 do
+        local Card = WidgetCards:Get(i)
+        local CardID = Card.CardID
+        local bHasCard = false
+        for j = 1, #Cards do
+            if Cards[j] == CardID then
+                bHasCard = true
+                break
+            end
+        end
+        Card:SetHave(bHasCard)
+    end
+end
+
 function UI_PlayerStory:OnDestroy()
     -- body
 end
