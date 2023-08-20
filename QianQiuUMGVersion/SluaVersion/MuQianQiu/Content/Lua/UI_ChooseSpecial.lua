6d9d2e9b5b18de6f1b670ef68bad210cb79c1f0e
木尘痕 <muchenhen@gmail.com>
2023-08-20 20:01:46 +0800

    初始化特殊卡界面

---
+++ QianQiuUMGVersion/SluaVersion/MuQianQiu/Content/Lua/UI_ChooseSpecial.lua
@@ -0,0 +1,36 @@
+require("Global")
+
+local UI_ChooseSpecial = {}
+
+function UI_ChooseSpecial:Initialize()
+    self.Button_Begin.OnClicked:Add(MakeCallBack(self.OnStartClick, self))
+
+    self.TileView_SpecialCards.BP_OnEntryInitialized:Add(MakeCallBack(self.TileView_SpecialCards_OnEntryInitialized, self))
+
+    self.AllSpecialCards = DataManager.GetAllSpecialCardDatas()
+    -- self.TileView_SpecialCards.
+    -- AllSpecialCards:Num()
+    for i=0, self.AllSpecialCards:Num()-1 do
+        local CardData = self.AllSpecialCards:Get(i)
+        local NewCard = MuBPFunction.CreateUserWidget("UI_Card")
+        NewCard:SetCardID(CardData.CardID)
+        self.TileView_SpecialCards:AddItem(NewCard)
+    end
+end
+
+function UI_ChooseSpecial:OnStartClick()
+    local UI_GameMain = MuBPFunction.CreateUserWidget("UI_GameMain")
+    UI_GameMain:AddToViewport(0)
+    self:SetVisibility(ESlateVisibility.Hidden)
+end
+
+function UI_ChooseSpecial:TileView_SpecialCards_OnEntryInitialized(Item, Widget)
+    -- local CardData = DataManager.GetCardData(Index)
+    Widget:SetCardID(Item.CardID)
+end
+
+function UI_ChooseSpecial:OnDestroy()
+
+end
+
+return Class(nil, nil, UI_ChooseSpecial)
