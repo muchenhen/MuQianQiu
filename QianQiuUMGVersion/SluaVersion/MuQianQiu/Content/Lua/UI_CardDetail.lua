d2baff45b704e2f21c09aa4bcafe6bcd1d18469e
木尘痕 <muchenhen@gmail.com>
2023-09-16 07:41:53 +0800

    添加特殊牌可完成的组合
    添加特殊牌可完成的组合
    调整UI

--- QianQiuUMGVersion/SluaVersion/MuQianQiu/Content/Lua/UI_CardDetail.lua
+++ QianQiuUMGVersion/SluaVersion/MuQianQiu/Content/Lua/UI_CardDetail.lua
@@ -21,7 +21,7 @@ function UI_CardDetail:SetCardID(CardID)
 
     self.UI_Card:SetCardID(self.CardID)
 
-    self.RelativeCardNames = self:GetRelativeCardNames(self.CardID)
+    self.RelativeCardNames = self:GetRelativeCardNames(self.SpecialName)
     self.StoryNames = self:GetStoryNames(self.CardID)
     self.SkillOneDesc = self:GetSkillDesc(self.SkillOneID, self.SkillOneParam)
     self.SkillTwoDesc = self:GetSkillDesc(self.SkillTwoID, self.SkillTwoParam)
@@ -29,12 +29,42 @@ function UI_CardDetail:SetCardID(CardID)
     self.Text_CardName:SetText(self.CardName)
     self.Text_CardSeason:SetText(self.Season)
     self.Text_CardValue:SetText(self.Value)
+    self.Text_RelativeCard:SetText(self.RelativeCardNames)
 end
 
 function UI_CardDetail:GetRelativeCardNames(CardID)
     local RelativeCardNames = {}
 
-    return RelativeCardNames
+    local AllStories = GameManager.AllStory
+    for key, value in pairs(AllStories) do
+        local CardsName = value.Story.CardsName
+        local CardsID = value.Story.CardsID
+        -- CardsID中是否包含CardID
+        for i=0, CardsID:Num()-1 do
+            if CardsID:Get(i) == CardID then
+                -- 将CardsName中的所有卡牌名字加入RelativeCardNames (不重复)
+                for j=0, CardsName:Num()-1 do
+                    local bExist = false
+                    for k=1, #RelativeCardNames do
+                        if RelativeCardNames[k] == CardsName:Get(j) then
+                            bExist = true
+                            break
+                        end
+                    end
+                    if not bExist then
+                        table.insert(RelativeCardNames, CardsName:Get(j))
+                    end
+                end
+            end
+        end
+    end
+
+    local RelativeCardNameString = ""
+    for i=1, #RelativeCardNames do
+        RelativeCardNameString = RelativeCardNameString .. RelativeCardNames[i] .. " "
+    end
+
+    return RelativeCardNameString
 end
 
 function UI_CardDetail:GetStoryNames(CardID)
