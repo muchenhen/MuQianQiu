548a79b278db3c476cd0d58f089790ca709d4a6c
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-08-28 21:23:29 +0800

    添加Card类型并进行复用性测试 重命名并导入部分Texture资源

--- GodotVersion/NetServerTest.gd
+++ GodotVersion/NetServerTest.gd
@@ -10,12 +10,12 @@ var connection_timer = 0.0
 var last_status = -1
 
 func _ready():
-	return
+	pass
 	print("Attempting to connect to %s:%d" % [host, port])
 	connect_to_server()
 
 func _process(delta):
-	return
+	pass
 	connection_timer += delta
 	
 	if connection_timer >= 0.5:  # 每0.5秒检查一次状态
---
+++ GodotVersion/Objects/Card.gd
@@ -0,0 +1,31 @@
+@tool
+extends Sprite2D
+
+@export var ID: int = 201
+@export var Name: String = "阿阮"
+@export var PinyinName: String = "ARuan":
+	set(value):
+		PinyinName = value
+		update_card()
+@export var Type: String = "2"
+@export var Score: int = 2
+@export var Season: String = "春"
+@export var BaseID: int = 201
+@export var Special: bool = false
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	update_card()
+
+func update_card() -> void:
+	if Engine.is_editor_hint():
+		_load_image()
+
+func _load_image() -> void:
+	var path = "res://Textures/Cards/" + Type + "/Tex_" + PinyinName + ".png"
+	var loaded_texture = load(path)
+	if texture:
+		self.texture = loaded_texture
+	else:
+		print("Failed to load texture: " + path)
