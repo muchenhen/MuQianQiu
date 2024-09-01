53cf02b7583e647426b33f9a1ce1b41df32c7a4e
muchenhen <44308876+muchenhen@users.noreply.github.com>
2024-09-01 22:38:16 +0800

    添加Player类型

---
+++ GodotVersion/Scripts/Objects/Player.gd
@@ -0,0 +1,19 @@
+extends Node
+
+class_name Player
+
+var player_name: String = "Player"
+
+var player_hand_cards = []
+
+var player_deal_cards = []
+
+var player_score: int = 0
+
+var player_finish_stories = []
+
+func _ready() -> void:
+    pass
+
+
+
